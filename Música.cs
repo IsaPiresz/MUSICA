5a1691762f6e7be84915852f0bb937b75893b5a0
IsaPiresz <isapiresz2304@gmail.com>
2025-03-07 21:42:45 -0300

    Add files via upload
    Aula 07/03

---
+++ Música.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Isabella Pires");
+
+// Mudar a cor de fundo
+// Vermelho
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write(" Birds of a Feather ");
+
+//Voltar a cor padrão
+Console.ResetColor();
+// Mudar cor da letra
+// Amarelo
+Console.ForegroundColor = ConsoleColor.Yellow;
+// String com várias linhas
+Console.Write(@"I want you to stay
+Till I'm in the grave
+Till I rot away, dead and buried
+Till I'm in the casket you carry
+
+If you go, I'm going too, uh
+'Cause it was always you, alright
+(And if I'm turning blue, please, don't save me)
+Nothing left to lose without my baby
+
+Birds of a feather, we should stick together
+I know I said I'd never think I wasn't better alone
+Can't change the weather, might not be forever
+But if it's forever, it's even better
+
+And I don't know what I'm crying for
+I don't think I could love you more
+It might not be long, but, baby, I ");
