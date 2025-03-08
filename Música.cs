730a791953b586bac0a3ac976e3655dbe79619aa
IsaPiresz <isapiresz2304@gmail.com>
2025-03-07 22:06:23 -0300

    Aula (Saída de Console) Música: Birds Of a Feather

--- Música.cs
+++ Música.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Isabella Pires");
+Console.WriteLine("Isabella Pires");
 
 // Mudar a cor de fundo
 // Vermelho
@@ -7,9 +7,10 @@ Console.Write(" Birds of a Feather ");
 
 //Voltar a cor padrão
 Console.ResetColor();
+
 // Mudar cor da letra
-// Amarelo
-Console.ForegroundColor = ConsoleColor.Yellow;
+// Branco
+Console.ForegroundColor = ConsoleColor.White;
 // String com várias linhas
 Console.Write(@"I want you to stay
 Till I'm in the grave
@@ -19,13 +20,21 @@ Till I'm in the casket you carry
 If you go, I'm going too, uh
 'Cause it was always you, alright
 (And if I'm turning blue, please, don't save me)
-Nothing left to lose without my baby
+Nothing left to lose without my baby ");
 
-Birds of a feather, we should stick together
+// Mudar cor da letra
+//Verde
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+// String com várias Linhas
+Console.Write(@"   Birds of a feather, we should stick together
 I know I said I'd never think I wasn't better alone
 Can't change the weather, might not be forever
-But if it's forever, it's even better
+But if it's forever, it's even better");
 
-And I don't know what I'm crying for
+// Mudar cor da letra
+// Branco
+Console.ForegroundColor= ConsoleColor.White;
+// String com várias linhas
+Console.Write(@"And I don't know what I'm crying for
 I don't think I could love you more
 It might not be long, but, baby, I ");
